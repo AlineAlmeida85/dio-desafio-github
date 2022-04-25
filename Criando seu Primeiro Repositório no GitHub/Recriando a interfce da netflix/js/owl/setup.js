15b95d22418c94030f826cda636a711bb954ed64
AlineAlmeida85 <aasouza20@hotmail.com>
2022-04-25 14:45:25 -0300

    Desafio Recriando a Interface do Netflix

---
+++ Criando seu Primeiro Repositório no GitHub/Recriando a interfce da netflix/js/owl/setup.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
